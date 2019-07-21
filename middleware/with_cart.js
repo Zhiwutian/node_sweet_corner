f957520f1a3d1cd47bdce42d0049f4a026fbc5e6
zhiwutian <brettalbright@outlook.com>
2019-07-20 18:02:38 -0700

    completed initial refactor of withcart

--- middleware/with_cart.js
+++ middleware/with_cart.js
@@ -8,54 +8,68 @@ module.exports = async (req, res, next) => {
        const { 'x-cart-token': cartToken } = req.headers;
        req.cart = null;
 
-       if (cartToken) {
-            const cartData = jwt.decode(cartToken, cartSecret);
+       const [[cartStatus = null]] = await db.query(
+           `SELECT id FROM cartStatuses WHERE mid="active"`
+       );
 
-            const [cart = null] = await db.query(
-                `SELECT c.id AS cartId, c.lastInteraction, c.pid, c.createdAt,
-                c.updatedAt, c.userId, c.statusId AS cartStatusId,
-                ci.quantity, p.cost, p.id AS productId FROM carts AS c
-                JOIN cartItems AS ci ON ci.cartId=c.id
-                JOIN products AS p ON ci.productId=p.id
-                WHERE c.id=${cartData.cartId} AND c.deletedAt IS NULL AND ci.deletedAt IS NULL`
-            );
-
-            if(!cart) {
-                throw new StatusError (422, "Invalid Cart Token!");
-            }
-
-            const {cost, quantity, productId, ...cartItem} = cart[0]
-
-            const formattedCart = {
-                ...cartItem,
-                items: cart.map(({cost,quantity, productId}) =>({cost, quantity, productId}))
-            };
-
-            // const formattedCart = {
-            //     ...cartItem,
-            //     items: cart.map((item) => {
-            //         return {
-            //             cost: item.cost,
-            //             quantity: item.quantity
-            //         }
-            //     })
-            // }
-            // cart.forEach(item => {
-                // formattedCart.id = item.id;
-                // formattedCart.lastInteraction = item.lastInteraction;
-                // formattedCart.pid = item.pid;
-                // formattedCart.createdAt = item.createdAt;
-                // formattedCart.userId = item.userId;
-                // formattedCart.cartStatudId = item.cartStatusId;
-
-                // formattedCart.items.push({
-                //     cost: item.cost,
-                //     quantity: item.quantity
-                // });
-            // });
-            req.cart = formattedCart;
+       if(!cartStatus) {
+           throw new StatusError(500, "Error retrieving cart data")
        }
 
+       const cartQuery = `SELECT c.id AS cartId, c.lastInteraction, c.pid, c.createdAt,
+       c.updatedAt, c.userId, c.statusId AS cartStatusId,
+       ci.quantity, p.cost, p.id AS productId FROM carts AS c
+       JOIN cartItems AS ci ON ci.cartId=c.id
+       JOIN products AS p ON ci.productId=p.id
+       WHERE c.deletedAt IS NULL AND ci.deletedAt IS NULL AND c.statusId = ${cartStatus.id} `;
+       let cartWhere = null;
+      if(req.user){
+        cartWhere = ` AND c.userId=${req.user.id}`
+
+
+      }else if (cartToken) {
+            const cartData = jwt.decode(cartToken, cartSecret);
+            cartWhere = ` AND c.id=${cartData.cartId}`
+
+       }
+       if(cartWhere){
+        const [cart=null] = await db.query(
+            cartQuery + cartWhere
+        );
+
+       if (cart) {
+        const {cost, quantity, productId, ...cartItem} = cart[0]
+
+        const formattedCart = {
+            ...cartItem,
+            items: cart.map(({cost,quantity, productId}) =>({cost, quantity, productId}))
+        };
+
+        // const formattedCart = {
+        //     ...cartItem,
+        //     items: cart.map((item) => {
+        //         return {
+        //             cost: item.cost,
+        //             quantity: item.quantity
+        //         }
+        //     })
+        // }
+        // cart.forEach(item => {
+            // formattedCart.id = item.id;
+            // formattedCart.lastInteraction = item.lastInteraction;
+            // formattedCart.pid = item.pid;
+            // formattedCart.createdAt = item.createdAt;
+            // formattedCart.userId = item.userId;
+            // formattedCart.cartStatudId = item.cartStatusId;
+
+            // formattedCart.items.push({
+            //     cost: item.cost,
+            //     quantity: item.quantity
+            // });
+        // });
+        req.cart = formattedCart;
+       }
+       }
         next();
 
     } catch (error) {
